82f1790c15a229e10d7c8ca947240a4b888f328c
bd <bborisdd@gmail.com>
2022-11-07 15:23:51 +0200

    initial commit

---
+++ src/main/java/com/boschgitrepos/BoschGitReposApplication.java
@@ -0,0 +1,13 @@
+package com.boschgitrepos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoschGitReposApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoschGitReposApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boschgitrepos/BoschGitReposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boschgitrepos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoschGitReposApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
